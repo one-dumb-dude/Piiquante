acbbfbc80b704aacd3cd9fb837d86ec00705f896
David Cotto <dacotto1984@gmail.com>
2023-08-11 19:28:43 -0400

    project seems complete.  Made a full turn around.

--- server/controllers/sauceController.js
+++ server/controllers/sauceController.js
@@ -1,9 +1,9 @@
 const Sauce = require('../models/Sauce.model');
+const mongoose = require('mongoose');
 
 function getSauces(req, res) {
   Sauce.find({})
     .then(sauce => {
-      console.log(sauce);
       res.status(201).send(sauce);
     })
     .catch(err => {
@@ -13,12 +13,8 @@ function getSauces(req, res) {
 }
 
 function getOneSauce(req, res) {
-  console.log(req.params)
-  console.log(req.params.id);
   Sauce.findById(req.params.id)
     .then((sauce) => {
-      console.log('you found a sauce');
-      console.log(sauce);
       res.status(201).json(sauce)
     })
     .catch(err => {
@@ -29,7 +25,6 @@ function getOneSauce(req, res) {
 
 // TODO: an image shouldnt save if theres an error
 function createOneSauce(req, res) {
-  console.log('creating a Salsa!');
   const sauceData = JSON.parse(req.body.sauce);
 
   const newSauce = new Sauce({
@@ -58,7 +53,6 @@ function updateOneSauce(req, res) {
   const id = req.params.id;
 
   if (req.body.sauce) {
-    console.log('Sauce with File!')
     const sauceData = JSON.parse(req.body.sauce);
     Sauce.updateOne(
       {_id: id},
@@ -122,24 +116,23 @@ function deleteASauce(req, res) {
 }
 
 function likeASauce(req, res) {
-  console.log('Like a sauce');
+  console.log('Sauce Liked!');
   console.log(req.body);
 
-  const id = req.params.id;
+  const sauceId = req.params.id;
   const userId = req.body.userId;
 
   if (req.body.like === 1) {
     Sauce.findOneAndUpdate(
-      {_id: id},
+      {_id: sauceId},
       {$push: {usersLiked: userId}},
       {new: true}
     )
       .then((updatedDocument) => {
         if (updatedDocument) {
-          console.log('Updated Document', updatedDocument);
 
           return Sauce.updateOne(
-            {_id: id},
+            {_id: sauceId},
             {
               $inc: {
                 likes: 1
@@ -149,6 +142,7 @@ function likeASauce(req, res) {
 
         } else {
           console.log('No matching document found.');
+          res.status(500).json({error: 'No matching document found'});
         }
       })
       .then((userLikedUpdate) => {
@@ -162,27 +156,81 @@ function likeASauce(req, res) {
         res.status(500).json({message: 'Error liking a sauce'})
       });
   } else if (req.body.like === 0) {
-    Sauce.updateMany(
+
+    const ObjectId = mongoose.Types.ObjectId;
+
+    const userObjId = new ObjectId(userId);
+
+    Sauce.findOneAndUpdate(
+      {_id: sauceId, usersLiked: userId},
       {
-        _id: id,
-        $or: [{usersLiked: userId}, {usersDisliked: userId}]
-      },
-      {
-        $pull: {
-          usersLiked: userId, usersDisliked: userId
-        }
+        $pull: {usersLiked: userId}
       }
-    )
-      .then((updatedLikes) => {
-        console.log('Like/dislike is now neutral');
-        res.status(201).json({message: 'Like/dislike is now neutral'});
-      })
-      .catch(err => {
-        res.status(500).json({error: err});
-      })
+    ).then(sauceLiked => {
+
+      if (sauceLiked) {
+        // objectId was present in userLikes and has been removed
+        Sauce.findByIdAndUpdate(sauceLiked._id, {$inc: {likes: -1}}).then(() => {
+          console.log("ObjectId found in userLikes and Likes decremented by 1");
+          res.status(201).json({message: 'ObjectId found in userLikes and Likes decremented by 1'});
+        });
+      } else {
+        // If objectId is not found in userLikes, proceed to check userDislikes
+        Sauce.findOneAndUpdate(
+          {_id: sauceId, usersDisliked: userId},
+          {
+            $pull: {usersDisliked: userId}
+          }
+        ).then(sauceDisliked => {
+          if (sauceDisliked) {
+            // objectId was present in userDislikes and has been removed
+            // Decrement the Dislikes count
+            Sauce.findByIdAndUpdate(
+              sauceDisliked._id,
+              {$inc: {dislikes: -1}}
+            )
+              .then(() => {
+                console.log("ObjectId found in userDislikes and Dislikes decremented by 1");
+                res.status(201).json({message: 'ObjectId found in userDislikes and Dislikes decremented by 1'});
+              });
+          } else {
+            console.log("ObjectId not found in both userLikes and userDislikes");
+            res.status(500).json({message: 'ObjectId not found in both userLikes and userDislikes'});
+          }
+        });
+      }
+    }).catch(error => {
+      console.error('Error occurred:', error);
+      res.status(500).json({error: error});
+    });
+
+  } else if (req.body.like === -1) {
+    console.log('Sauce disliked!');
+    Sauce.findOneAndUpdate(
+      {_id: sauceId},
+      {$push: {usersDisliked: userId}},
+      {new: true}
+    ).then(updatedDocument => {
+      if (updatedDocument) {
+        Sauce.updateOne(
+          {_id: sauceId},
+          {$inc: {dislikes: 1}}
+        )
+          .then(() => {
+            console.log('Disliked successful');
+            res.status(201).json({message: 'Disliked successful!'});
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            res.status(500).json({error: error})
+          });
+      }
+    })
+      .catch(error => {
+        console.error('Error: ', error);
+        res.status(500).json({error: error});
+      });
   }
-
-
 }
 
 module.exports = {
